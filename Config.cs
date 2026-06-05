7941fc8a841a7b4f17a179ded2f63795c3e920eb
pereverzeva.polina08@gmail.com <pereverzeva.polina08@gmail.com>
2026-06-05 21:45:53 +0300

    # Добавлен класс настроек

---
+++ Config.cs
@@ -0,0 +1,8 @@
+public static class AppSettings
+{
+    public static string AppName = "MyApplication";
+    public static string Version = "1.0.0";
+    public static int MaxRetryCount = 3;
+    public static bool EnableLogging = true;
+    public static string LogPath = "logs/app.log";
+}
